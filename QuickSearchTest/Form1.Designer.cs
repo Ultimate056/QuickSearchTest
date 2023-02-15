06d2002d54d48d6b21e014de4716214dc1aeb54d
KopylovVU <KopylovVU@SQL3TSRV>
2023-02-15 16:38:43 +0300

    Добавьте файлы проекта.

---
+++ QuickSearchTest/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace QuickSearchTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(22, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(584, 223);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(751, 12);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(351, 223);
+            this.dataGridView2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(639, 204);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 31);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Токенезировать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(23, 254);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(423, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(466, 252);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Search";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 289);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "label1";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(22, 327);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(584, 186);
+            this.listBox1.TabIndex = 6;
+            this.listBox1.Enter += new System.EventHandler(this.listBox1_Enter);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 302);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(748, 252);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "label3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1114, 522);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ QuickSearchTest/Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace QuickSearchTest
+{
+    public partial class Form1 : Form
+    {
+        public class PairToken
+        {
+            public int _idTov { get; private set; }
+
+            public string _token { get; private set; }
+
+            public PairToken(int idTov, string tok)
+            {
+                _idTov = idTov;
+                _token = tok;
+            }
+        }
+
+        public class PairMatch
+        {
+            public int _idTov { get; private set; }
+
+            public int _count { get; private set; }
+
+            public PairMatch(int idTov, int count)
+            {
+                _idTov = idTov;
+                _count = count;
+            }
+        }
+
+        readonly string _connectionString;
+        List<PairToken> _tokensList;
+        int _lengthOfToken=3;
+        Stopwatch _timer;
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //Заполняем табличку на форме видами товара
+            _connectionString = @"Server=DBSRV\DBSRV;Database=test;Integrated Security=SSPI;Connect Timeout=600";
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                string Query = @"select  st.id_tov, st.n_tov 
+                                        from spr_tov_level4 l4(nolock)
+										inner join spr_tov st(nolock) on st.id_tov4 =l4.tov_id
+                                        where tov_id_top_level!=0
+                                        order by tov_id_top_level
+                                        ";
+
+                DataSet ds = new DataSet();
+                SqlDataAdapter da = new SqlDataAdapter(Query, connection);
+                da.SelectCommand.CommandTimeout = 0;
+                da.Fill(ds);
+ 
+                dataGridView1.DataSource=  ds.Tables[0];
+
+                connection.Close();
+
+                _timer = new Stopwatch();
+            }
+        }
+
+        //Токенезируем все наименования в исходных данных
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _tokensList = new List<PairToken>();
+            DataTable rawDataTable = (DataTable)dataGridView1.DataSource;
+
+            foreach(DataRow row in rawDataTable.Rows)
+            {
+                int idTov = Convert.ToInt32(row.ItemArray.First());
+                string normalizedNameTov = NormalizeString(row.ItemArray.Last());
+                List<string> tokenByWord = TokenizeWord(normalizedNameTov);
+
+                foreach(var token in tokenByWord)
+                {
+                    try
+                    {
+                        var pair = new PairToken(idTov, token);
+                        _tokensList.Add(pair);
+                    }
+                    catch(Exception ex)
+                    {
+                    }
+                }
+            }
+
+            dataGridView2.DataSource = _tokensList.ToArray();
+
+            label3.Text = $"Общее количество токенов: {_tokensList.Count}";
+        }
+
+        private string NormalizeString(object str)
+        {        
+            string res= str.ToString().Trim().ToLower();
+            res = DelSymbols(res, 'а', 'я', 'у', 'ю', 'о', 'е', 'ё', 'э', 'и', 'ы','ь','ъ');
+            res = DelSymbols(res, '0', '1', '2', '3', '4', '5', '6', '7', '8', '9');
+            return DelSymbols(res, '/', '.', '-', ' ', '\\', '|', '&', '^', '*', ';', ':', '>', '<', ',', '+', '"', '?', '=', '(', ')',' ');
+        }
+
+        private string DelSymbols(string s, params char[] par)
+        {
+            foreach (var p in par)
+                s = s.Replace(p.ToString(), "");
+
+            return s;
+        }
+
+        private List<string> TokenizeWord(string word)
+        {
+            var listOfResult = new List<string>();
+
+            if (word.ToArray().Length <= _lengthOfToken)
+            {
+                listOfResult.Add(word);
+            }
+            else
+            {
+                for(int i=0; i< word.ToArray().Length-_lengthOfToken+1;i++)
+                {
+                    string str = word.Substring(i, _lengthOfToken);
+                    listOfResult.Add(str);
+                    //char[] charArray = str.ToCharArray();
+                    //Array.Reverse(charArray);
+                    //listOfResult.Add(new string(charArray));
+                }
+            }
+
+            return listOfResult;
+        }
+
+        //Выполнить поиск по слову из ввода
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (_tokensList != null)
+            {
+                _timer.Start();
+
+                //Нормализуем ввод и разбиваем его на токены
+                var word = NormalizeString(textBox1.Text);
+                var listOfTokens = TokenizeWord(word);
+
+                //Ищем по существующим токенам пары с токенами из запроса
+                List<PairToken> resultPair = new List<PairToken>();
+                resultPair = (from t in _tokensList
+                              join l in listOfTokens on t._token equals l
+                              orderby t._idTov
+                              select t).ToList();
+
+                //Выделяем все Id из обнаруженных пар
+                IEnumerable<int> listOfId = (from id in resultPair
+                                             select id._idTov);
+
+                //Подсчитываем количество совпадений токенов по id
+                var resultMatch = listOfId.GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count());
+
+
+                //Выбираем id по максимальному количеству совпадений
+                var idMaxMatch = (from m in resultMatch
+                                  orderby m.Value descending
+                                  select m).Take(10);
+
+                //Ищем в датасорсе наименование по id
+                DataTable rawDataTable = (DataTable)dataGridView1.DataSource;
+                string resultName = "";
+
+                List<string> listOfResult = (from id in rawDataTable.AsEnumerable()
+                                             join match in idMaxMatch on id.ItemArray.FirstOrDefault().ToString() equals match.Key.ToString()
+                                             select id
+                                                      .ItemArray
+                                                      .LastOrDefault()
+                                                      .ToString())
+                                                           .ToList();
+
+                resultName = listOfResult.FirstOrDefault();
+
+                //Выводим id на форму
+                label1.Text = resultName;
+                listBox1.DataSource = listOfResult;
+
+
+
+                _timer.Stop();
+
+                TimeSpan timeTaken = _timer.Elapsed;
+                label2.Text = "Time taken: " + timeTaken.ToString(@"m\:ss\.fff");
+                _timer.Reset();
+            }
+            else
+                MessageBox.Show("Сначала надо токенизировать слово");
+            
+        }
+
+        private void listBox1_Enter(object sender, EventArgs e)
+        {
+            var item= listBox1.SelectedItem.ToString();
+            textBox1.Text = item;
+        }
+    }
+}
